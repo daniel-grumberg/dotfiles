64d63c459dcde6693b9f258f8c29f2a304202fc9
Daniel Grumberg <dany.grumberg@gmail.com>
2021-02-16 14:30:37 +0000

    Add ssh socket variable to bash_profile on arch

--- bash/bash_profile_arch.sh
+++ bash/bash_profile_arch.sh
@@ -1,5 +1,7 @@
 [[ -f "${HOME}/.bashrc" ]] && source "${HOME}/.bashrc"
 
+export SSH_AUTH_SOCK="$XDG_RUNTIME_DIR/ssh-agent.socket"
+
 if [[ ! "${DISPLAY}" && "${XDG_VTNR}" -eq "1" ]]; then
     exec startx
 fi
