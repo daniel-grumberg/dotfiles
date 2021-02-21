6067c3dcb4c250e87cbce60be34c6a4d6b950284
Daniel Grumberg <dany.grumberg@gmail.com>
2021-02-21 00:09:09 +0000

    Ensure emacs install goes smoothly
    Add code to install use-package from MELPA if not already there.
    Add code to post installs to install dependencies for pdf-tools and all-the-icons.

--- emacs/custom-config/package-management.el
+++ emacs/custom-config/package-management.el
@@ -1,8 +1,15 @@
 (require 'package)
+(setq package-enable-at-startup nil)
 (add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
+(package-initialize)
+
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
 
 (eval-when-compile
   (require 'use-package))
+
 (require 'use-package-ensure)
 (setq use-package-always-ensure t)
 
