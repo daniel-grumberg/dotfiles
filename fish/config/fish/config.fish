6199b66c0d37c7220017530615f4cd5d05ee0857
Daniel Grumberg <dgrumberg@apple.com>
2022-06-29 15:07:22 +0100

    Make sure that PATH components inside /Users/dgrumberg are correctly set across machines.

--- fish/config/fish/config.fish
+++ fish/config/fish/config.fish
@@ -1,4 +1,8 @@
 if status is-interactive
-    # Commands to run in interactive sessions can go here
+    # Initialize fuck
     thefuck --alias | source
+    # Make sure that PATH components inside the home directory are correctly
+    # set. This can not use $fish_user_paths because my home directory is not
+    # consistent across machines.
+    set PATH ~/.local/src/arcanist/bin ~/.local/bin $PATH
 end
