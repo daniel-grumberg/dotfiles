789f5a5470f9ed9cfd6e2e3427210cd603875e32
Daniel Grumberg <dgrumberg@apple.com>
2022-07-01 10:17:47 +0100

    Ensure PATH is properly set with ~/.local

--- fish/config/fish/config.fish
+++ fish/config/fish/config.fish
@@ -1,8 +1,9 @@
+# Make sure that PATH components inside the home directory are correctly
+# set. This can not use $fish_user_paths because my home directory is not
+# consistent across machines.
+set -gx PATH ~/.local/src/arcanist/bin ~/.local/bin $PATH
+
 if status is-interactive
     # Initialize fuck
     thefuck --alias | source
-    # Make sure that PATH components inside the home directory are correctly
-    # set. This can not use $fish_user_paths because my home directory is not
-    # consistent across machines.
-    set PATH ~/.local/src/arcanist/bin ~/.local/bin $PATH
 end
