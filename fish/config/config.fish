d1cd1b1243a2f5c8df461b15009fe105e3285867
Daniel Grumberg <dgrumberg@apple.com>
2022-07-01 10:47:33 +0100

    Fix fish directory layout

--- fish/config/config.fish
+++ fish/config/config.fish
@@ -1,3 +1,9 @@
+# Make sure that PATH components inside the home directory are correctly
+# set. This can not use $fish_user_paths because my home directory is not
+# consistent across machines.
+set -gx PATH ~/.local/src/arcanist/bin ~/.local/bin $PATH
+
 if status is-interactive
-    # Commands to run in interactive sessions can go here
+    # Initialize fuck
+    thefuck --alias | source
 end
--- fish/config/fish/config.fish
+++
@@ -1,9 +0,0 @@
-# Make sure that PATH components inside the home directory are correctly
-# set. This can not use $fish_user_paths because my home directory is not
-# consistent across machines.
-set -gx PATH ~/.local/src/arcanist/bin ~/.local/bin $PATH
-
-if status is-interactive
-    # Initialize fuck
-    thefuck --alias | source
-end
