513140740b0158f42c224f53d3b098bbd1223fd8
Daniel Grumberg <dgrumberg@apple.com>
2022-08-30 15:24:10 +0100

    Updates to fish setup

--- fish/config/config.fish
+++ fish/config/config.fish
@@ -1,12 +1,13 @@
 # Make sure that PATH components inside the home directory are correctly
 # set. This can not use $fish_user_paths because my home directory is not
 # consistent across machines.
-set -gx PATH ~/.local/src/arcanist/bin ~/.local/bin $PATH
+set -x PATH ~/.local/src/arcanist/bin ~/.local/bin $PATH
 
 set -gx NVM_DIR ~/.nvm
 function nvm
   bass source ~/.nvm/nvm.sh --no-use ';' nvm $argv
 end
+
 nvm use default --silent
 
 if status is-interactive
