2bd5501072381703123cea6581c4b4c7a1be7fa4
Daniel Grumberg <dgrumberg@apple.com>
2024-04-15 14:18:47 +0100

    Remove custom gitsigns and rely on defaults.

--- neovim/lua/plugins/git.lua
+++ neovim/lua/plugins/git.lua
@@ -4,14 +4,6 @@ return {
 		event = { 'BufReadPre', 'BufNewFile' },
 		cmd = 'Gitsigns',
 		opts = {
-			signs = {
-				add = { text = '▎' },
-				change = { text = '▎' },
-				delete = { text = '' },
-				topdelete = { text = '' },
-				changedelete = { text = '▎' },
-				untracked = { text = '▎' },
-			},
 			on_attach = function(buffer)
 				local gs = package.loaded.gitsigns
 
