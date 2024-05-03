1522abe50a1030039f8b3dba2d623efd777a6af3
Daniel Grumberg <dgrumberg@apple.com>
2024-05-04 00:23:55 +0100

    Stop exporting fish_function_path variable
    This doesn't work as expected as the variable is exported to child
    processes as a single string instead of an array. This was not necessary
    anyway since fish_funtcion_path only makes sense in fish contexts and
    config.fish is always executed even for non interactive invocations and
    login shells.

--- fish/config/config.fish
+++ fish/config/config.fish
@@ -1,8 +1,8 @@
 # Make sure that PATH components inside the home directory are correctly
 # set. This can not use $fish_user_paths because my home directory is not
 # consistent across machines.
-fish_add_path -g -m ~/.local/bin
-set -x fish_function_path ~/.local/share/fish/functions $fish_function_path
+fish_add_path -g -p ~/.local/bin
+set -p fish_function_path ~/.local/share/fish/functions
 
 set -gx NVM_DIR ~/.nvm
 function nvm
