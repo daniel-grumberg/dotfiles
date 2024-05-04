78b1d34ac70493e7795f2415848885a41435d1c2
Daniel Grumberg <dgrumberg@apple.com>
2024-05-04 08:38:55 +0100

    Split config.fish into snippets in conf.d

--- fish/config/conf.d/00_paths.fish
+++ fish/config/conf.d/00_paths.fish
@@ -3,16 +3,3 @@
 # consistent across machines.
 fish_add_path -g -p ~/.local/bin
 set -p fish_function_path ~/.local/share/fish/functions
-
-set -gx NVM_DIR ~/.nvm
-function nvm
-  bass source ~/.nvm/nvm.sh --no-use ';' nvm $argv
-end
-
-nvm use default --silent
-
-if status is-interactive
-    # Initialize fuck
-    thefuck --alias | source
-end
-
---
+++ fish/config/conf.d/nvm.fish
@@ -0,0 +1,6 @@
+set -gx NVM_DIR ~/.nvm
+function nvm
+  bass source ~/.nvm/nvm.sh --no-use ';' nvm $argv
+end
+
+nvm use default --silent
