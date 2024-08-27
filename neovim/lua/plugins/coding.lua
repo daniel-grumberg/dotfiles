04ecbd7a267e53b681b86f2be80c09806e5cc39a
Daniel Grumberg <dgrumberg@apple.com>
2024-08-27 15:24:17 +0100

    Update mini.comment configure to use native treesitter integration and provide sensible value for swift

--- neovim/lua/plugins/coding.lua
+++ neovim/lua/plugins/coding.lua
@@ -98,18 +98,14 @@ return {
 	},
 	-- comments
 	{
-		'JoosepAlviste/nvim-ts-context-commentstring',
-		lazy = true,
-		opts = {
-			enable_autocmd = false,
-		},
-	},
-	{
 		'echasnovski/mini.comment',
 		event = 'VeryLazy',
 		opts = {
 			options = {
 				custom_commentstring = function()
+          if vim.bo.filetype == 'swift' then
+            return '// %s'
+          end
 					return require('ts_context_commentstring.internal').calculate_commentstring() or vim.bo.commentstring
 				end,
 			},
