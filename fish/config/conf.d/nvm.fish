6cd17c91cbed9896cebae4484590b4731e420cd5
Daniel Grumberg <dany.grumberg@gmail.com>
2024-09-15 08:31:31 +0100

    Create nvm function only if there is an nvm script available

--- fish/config/conf.d/nvm.fish
+++ fish/config/conf.d/nvm.fish
@@ -1,6 +1,8 @@
 set -gx NVM_DIR ~/.nvm
-function nvm
-  bass source ~/.nvm/nvm.sh --no-use ';' nvm $argv
-end
+if test -x "{$NVM_DIR}/nvm.sh"
+  function nvm
+    bass source ~/.nvm/nvm.sh --no-use ';' nvm $argv
+  end
 
-nvm use default --silent
+  nvm use default --silent
+end
