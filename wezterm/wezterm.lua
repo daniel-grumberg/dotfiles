b898972cd751c02168ef45ed6d8b2332a297cae0
Daniel Grumberg <dgrumberg@apple.com>
2024-11-18 10:33:09 +0000

    [wezterm] Clean up config file

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,19 +1,11 @@
 -- Pull in the wezterm API
 local wezterm = require 'wezterm'
-
--- This will hold the configuration.
 local config = wezterm.config_builder()
 
--- This is where you actually apply your config choices
-
--- For example, changing the color scheme:
+-- General appearence
 config.color_scheme = 'catppuccin-mocha'
-
--- Font
 config.font = wezterm.font 'Iosevka Fixed'
 config.font_size = 15
-
--- General appearence
 config.enable_scroll_bar = true
 config.use_fancy_tab_bar = false
 config.hide_tab_bar_if_only_one_tab = true
@@ -47,5 +39,5 @@ config.mouse_bindings = {
     action = wezterm.action.CompleteSelectionOrOpenLinkAtMouseCursor('Clipboard'),
   },
 }
--- and finally, return the configuration to wezterm
+
 return config
