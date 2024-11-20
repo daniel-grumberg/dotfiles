098675400943b8b0be68c87cfceb2b8490a6e885
Daniel Grumberg <dgrumberg@apple.com>
2024-11-20 11:48:22 +0000

    [fish] add go installation path to user search path

--- fish/config/conf.d/00_paths.fish
+++ fish/config/conf.d/00_paths.fish
@@ -1,5 +1,5 @@
 # Make sure that PATH components inside the home directory are correctly
 # set. This can not use $fish_user_paths because my home directory is not
 # consistent across machines.
-fish_add_path -g -p ~/.local/bin
+fish_add_path -g -p ~/.local/bin ~/go/bin
 set -p fish_function_path ~/.local/share/fish/functions
