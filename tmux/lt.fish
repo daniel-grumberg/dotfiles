d91baea85b8d9ae4700ddda52d79f77bc239fdf9
Daniel Grumberg <dgrumberg@apple.com>
2024-11-23 01:32:40 +0000

    [tmux] Improve lt function
    - Remove duplicates by using awk
    - Ensure "official" projects are listed ahead of any zoxide results

--- tmux/lt.fish
+++ tmux/lt.fish
@@ -1,22 +1,19 @@
 function _mru_tmux_sessions -d "PRIVATE: list tmux sessions in most recent first order"
-  if test -n "$TMUX"
-    set -l current_sid (tmux display-message -p "#{session_id}" 2>&1 | grep -v "no server")
-  else
-    return
-  end
-
-  tmux list-sessions \
-    -f "#{!=:#{session_id},$current_sid}" \
-    -F '#{session_last_attached} #{session_name}' \
-    | sort --numeric-sort --reverse | awk '{print $2};'
+  tmux list-sessions -F '#{session_last_attached} #S' | sort --numeric-sort --reverse | awk '{print $2}'
 end
 
 function lt -d "List tmux sessions available and that can be started"
   # List non-currently attached sessions
   set tmux_sessions (_mru_tmux_sessions | string collect)
   # Make sure zoxide knows about official projects
-  find ~/VersionControlledDocuments -mindepth 1 -maxdepth 2 -type d | xargs -n 1 zoxide add
+  set official_projects (find ~/VersionControlledDocuments -mindepth 2 -maxdepth 2 -type d | string collect)
+  for proj in $official_projects
+    zoxide add "$proj"
+  end
   set zoxide_paths (zoxide query -l | string collect)
   # unique using awk. seen acts as a set of lines
-  echo "$tmux_sessions\n$zoxide_paths" | awk '!seen[$0]++'
+  if test -n "$TMUX"
+    set -f current_session_name (tmux display-message -p "#{session_name}")
+  end
+  echo "$tmux_sessions\n$official_projects\n$zoxide_paths" | awk -v name="$current_session_name" 'BEGIN { seen[name]++ } !seen[$0]++'
 end
